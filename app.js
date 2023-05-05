8740f20eee31239782a5323f55f3a4c17761ff68
Jose Silva <zeeeee.miguel@gmail.com>
2023-05-06 00:44:18 +0100

    changes to bilhetes page

--- app.js
+++ app.js
@@ -56,4 +56,28 @@ document.addEventListener("DOMContentLoaded", function(e) {
         });
 
     });
+
+
+	var bilhetes = document.getElementsByClassName("info");
+	var bilhetesBotoes = document.getElementsByClassName("bilheteLink");
+
+	for (let i = 0; i < bilhetes.length; i++) {
+		bilhetes[i].addEventListener("click", function () {
+			/* Toggle between adding and removing the "active" class,
+			to highlight the button that controls the panel */
+			this.classList.toggle("active");
+			/* Toggle between hiding and showing the active panel */
+			var body = this.nextElementSibling;
+			if (body.style.display === "block") {
+				body.style.display = "none";
+				if (i<3) bilhetesBotoes[i].style.display = "none";
+			} else {
+				body.style.display = "block";
+				if (i<3) bilhetesBotoes[i].style.display = "block";
+			}
+		});
+	}
+
+
 });
+
