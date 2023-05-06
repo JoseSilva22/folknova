87b4f7e64b6472f0d1ea5125cbbc5525001181ab
Pedro Nuno Silva <pedrobonitao22@gmail.com>
2023-05-06 19:54:47 +0100

    logos bandas

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let colors = ["#f04f41", "#4444da", "#69c49e"];
 
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", function (e) {
 
 
     let randColor = colors[Math.floor(Math.random() * colors.length)];
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     });
 
     bLinks.forEach(bLinks => {
-        bLinks.style.border = "5px solid" +randColor;
+        bLinks.style.border = "5px solid" + randColor;
         bLinks.style.backgroundColor = randColor;
     });
 
@@ -34,13 +34,13 @@ document.addEventListener("DOMContentLoaded", function(e) {
         var vpHeight = window.innerHeight;
 
         // Image Position
-        var xPos = getRandomInt(0, -element.offsetWidth/2);
+        var xPos = getRandomInt(0, -element.offsetWidth / 2);
         var yPos = getRandomInt(0, vpHeight - element.offsetHeight);
         var zIndex = getRandomInt(0, 13);
 
         //element.style.cssText += '--x-position:' + xPos + 'px; --y-position:' + yPos + 'px; z-index:' + zIndex;
 
-        if(Math.random()<0.5)
+        if (Math.random() < 0.5)
             element.style.left = xPos + 'px';
         else
             element.style.right = xPos + 'px';
@@ -63,28 +63,10 @@ document.addEventListener("DOMContentLoaded", function(e) {
     });
 
 
-	var bilhetes = document.getElementsByClassName("info");
-	var bilhetesBotoes = document.getElementsByClassName("bilheteLink");
-
-	for (let i = 0; i < bilhetes.length; i++) {
-		bilhetes[i].addEventListener("click", function () {
-			/* Toggle between adding and removing the "active" class,
-			to highlight the button that controls the panel */
-			this.classList.toggle("active");
-			/* Toggle between hiding and showing the active panel */
-			var body = this.nextElementSibling;
-
-
-			if (body.style.display === "block") {
-				body.style.display = "none";
-				if (i<3) bilhetesBotoes[i].style.display = "none";
-			} else {
-				body.style.display = "block";
-				if (i<3) bilhetesBotoes[i].style.display = "block";
-			}
-		});
-	}
-
+    $(".tituloBilhetes").click(function () {
+        console.log("asd")
+        $(this).children().toggleClass('main');
+    });
 
 });
 
